c8f6c53051d1ed567e180583a0b6a50f0c6cf11d
Hooby <hooby11105@naver.com>
2025-04-09 23:04:35 +0900

    test: Servlet is well Operated(need to expand functionalities)

--- src/main/java/com/hooby/Main.java
+++ src/main/java/com/hooby/Main.java
@@ -1,7 +1,27 @@
 package com.hooby;
 
+import com.hooby.servlet.TestServlet;
+import com.hooby.servlet.ServletContainer;
+import com.hooby.servlet.UserServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            ServletContainer container = ServletContainer.getInstance();
+
+            // /hello 경로에 대한 Servlet 등록
+            container.registerServlet("/hello", new TestServlet());
+            container.registerServlet("/users", new UserServlet());
+
+            // 서버 실행
+            container.start(8080); // 브라우저에서 http://localhost:8080/hello 로 접속 가능
+
+        } catch (Exception e) {
+            logger.error("🔴 뭔가 예기치 못한 에러가 발생했습니다.", e);
+        }
     }
 }
