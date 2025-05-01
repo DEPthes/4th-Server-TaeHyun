2cb5d98edfdfaf31f2cfd90defc7bc7035afebd2
Hooby <hooby11105@naver.com>
2025-05-01 17:55:40 +0900

    feat: Add LoggingFilter for Debugging

---
+++ src/main/java/com/hooby/servlet/LoggingFilter.java
@@ -0,0 +1,17 @@
+package com.hooby.servlet;
+
+import com.hooby.http.CustomHttpRequest;
+import com.hooby.http.CustomHttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public void doFilter(CustomHttpRequest request, CustomHttpResponse response, FilterChain chain) {
+        logger.info("→ [FILTER] Incoming request: {} {}", request.getMethod(), request.getPath());
+        chain.doFilter(request, response);
+        logger.info("← [FILTER] Response status: {}", response.toHttpMessage().split(" ")[1]);
+    }
+}
